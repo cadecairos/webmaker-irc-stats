f9f21d52ae7c84172796969293277fb2237d7254
Christopher De Cairos <chris.decairos@gmail.com>
2012-12-20 14:22:29 -0500

    change irc module to https://github.com/martynsmith/node-irc

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var VERSION = '0.1.0',
     USERNAME = 'WebmakerMetrics',
     REALNAME = 'USER';
 
-var nodeIRC = require( 'node-irc' ),
+var irc = require( 'irc' ),
     fs = require( 'fs' ),
     server = 'irc.mozilla.org',
     port = 6667,
@@ -13,28 +13,28 @@ var nodeIRC = require( 'node-irc' ),
     versionRegex = new RegExp( USERNAME + '(?::|:\\s|\\s)!version', 'i' ),
     client;
 
-function logMessage( data ) {
+function logMessage( nick, to, text ) {
   fs.appendFileSync(
-    logDir + data.receiver,
-    data.sender + ' ' + Date.now() + ' ' + data.message + '\n', 'utf-8',
+    logDir + to,
+    nick + ' ' + Date.now() + ' ' + text + '\n', 'utf-8',
     function( err ) {
       if ( err ) {
       console.log( err );
     }
   });
 
-  if ( pingRegex.test( data.message ) ) {
-    client.say( data.receiver, 'Hello ' + data.sender + '. I\'m not a real person, I just log channel messages. The logs are used to generate metrics data for the Webmaker dashboard. PS: Computers rule, Humans drool.' );
-  } else if ( versionRegex.test( data.message ) ) {
-    client.say( data.receiver, 'This is WebmakerMetrics bot v' + VERSION + '. Visit https://github.com/cadecairos/webmaker-irc-stats for the source code.' );
+  if ( pingRegex.test( text ) ) {
+    client.say( to, 'Hello ' + nick + '. I\'m not a real person, I just log channel messages. The logs are used to generate metrics data for the Webmaker dashboard. PS: Computers rule, Humans drool.' );
   }
 }
 
 function connect() {
-  client = new nodeIRC( server, port, USERNAME, REALNAME );
-  client.on( 'ready', joinChannels );
-  client.on( 'CHANMSG', logMessage );
-  client.on( 'close', connect );
+  client = new irc.Client( server, USERNAME, {
+    port: port,
+    autoConnect: false
+  });
+  client.on( 'registered', joinChannels );
+  client.on( 'message#', logMessage );
   client.connect();
 }
 
